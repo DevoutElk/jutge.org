fe70a3a681bd109492c511f44b3003ea3c06bfdc
DevoutElk <eliassver01@gmail.com>
2025-01-14 18:44:29 +0100

    Revert "fallo"
    This reverts commit d834fb3e857f1a0be573b9d878977195876b3dab.

---
+++ Codewars_2017/IO & Simple Math/Run or die (X20116).py
@@ -0,0 +1,4 @@
+kilometros = int(input("")) # Total de kilometros recorridos
+vueltas = int(input("")) # Cuantos kilometros recorre sin parar
+
+print (int(kilometros // vueltas )) # Numero de paradas que hace
