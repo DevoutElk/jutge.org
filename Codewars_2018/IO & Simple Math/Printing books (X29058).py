25954b470b04cb8c20a270af0fb35846ac2ec525
DevoutElk <eliassver01@gmail.com>
2025-01-14 19:51:35 +0100

    Printing-books

--- Codewars_2018/IO & Simple Math/Printing books (X29058).py
+++ Codewars_2018/IO & Simple Math/Printing books (X29058).py
@@ -1,7 +1,7 @@
 # X29058
 # Printing books
 # 'https://jutge.org/problems/X29058_en/'
-umero_paginas = int(input()) #cuantas paginas quieres imprimir
+numero_paginas = int(input()) #cuantas paginas quieres imprimir
 ink_cartucho = int(input()) #Numero de cartuchos de tinta
 tinta_necesaria = int(3) #cuanta tinta necesita una pagina
 
