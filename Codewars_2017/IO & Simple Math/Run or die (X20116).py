d834fb3e857f1a0be573b9d878977195876b3dab
DevoutElk <eliassver01@gmail.com>
2025-01-14 18:42:25 +0100

    fallo

--- Codewars_2017/IO & Simple Math/Run or die (X20116).py
+++
@@ -1,4 +0,0 @@
-kilometros = int(input("")) # Total de kilometros recorridos
-vueltas = int(input("")) # Cuantos kilometros recorre sin parar
-
-print (int(kilometros // vueltas )) # Numero de paradas que hace
