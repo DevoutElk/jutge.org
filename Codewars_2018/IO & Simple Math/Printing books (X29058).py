26d1cf1d83d0f0d9c38735834b102e5b1cd83ea4
DevoutElk <111908519+DevoutElk@users.noreply.github.com>
2025-01-14 18:46:19 +0100

    Delete Codewars_2018 directory

--- Codewars_2018/IO & Simple Math/Printing books (X29058).py
+++
@@ -1,9 +0,0 @@
-# X29058
-# Printing books
-# 'https://jutge.org/problems/X29058_en/'
-umero_paginas = int(input()) #cuantas paginas quieres imprimir
-ink_cartucho = int(input()) #Numero de cartuchos de tinta
-tinta_necesaria = int(3) #cuanta tinta necesita una pagina
-
-print(ink_cartucho-(numero_paginas*tinta_necesaria)) #cuanta tinta le queda al cartucho
-
--- Codewars_2018/IO & Simple Math/Savings for the Fallas (X21639).py
+++
@@ -1,7 +0,0 @@
-#X21639
-#Savings for the Fallas
-# 'https://jutge.org/problems/X21639_en'
-one_way_ticket_cost = int(input("")) # Cuanto le cuesta un billete de ida
-precio_ticket_vuelta = int(input("")) # Cuanto le cuesta un billete de vuelta
-
-print("Ana should save a total of", one_way_ticket_cost+precio_ticket_vuelta, "euros.") # Ana debería ahorrar un total de euros.
